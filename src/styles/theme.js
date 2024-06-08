59edb7b681dd454b7a1fa7061d5973a29cd46124
Guilherme oliveira de santana <san835117@gmail.com>
2024-06-08 18:11:45 +0000

    style: estilização do h1 e do p do card

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,7 +4,7 @@ homeBackground: "#131518",
 cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
 textColor: "#969FAD",
 submitButtonColor: "#FC7614",
-white: "FFF",
+white: "#FFF",
 feedbackButtonColor: "#262E38",
 feedbackButtonText: "#7C8798"
 
